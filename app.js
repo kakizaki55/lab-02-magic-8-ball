dac416ef86e1515c18510cae88ad94c7a7b854c9
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-05 13:47:15 -0700

    trying to get the text area to clear upon pressig the button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { answers } from './answer.js';
 
 const ballAnswer = document.getElementById('eight-ball-answer');
 const askButton =document.getElementById('ask-button');
+const questionArea = document.getElementById('quest');
 
 function randomNumber(){
   return Math.floor(Math.random()* answers.length);
@@ -13,9 +14,11 @@ function randomNumber(){
 // console.log(randomIndex);
 // console.log(answers[randomIndex])
 askButton.addEventListener('click', ()=>{
+
   let randomIndex = randomNumber()
   ballAnswer.textContent = answers[randomIndex] 
-})
+  questionArea.textContent = '';
+});
 
 
 // ballAnswer.textContent = answers[randomIndex] 
