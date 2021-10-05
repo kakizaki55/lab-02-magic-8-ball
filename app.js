180a2b70d92891d7f8580e5b7f7c7f1afcb93133
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-05 16:25:29 -0700

    all done for now

--- app.js
+++ app.js
@@ -14,26 +14,26 @@ let askCount = 0;
 // console.log(randomIndex);
 // console.log(answers[randomIndex])
 askButton.addEventListener('click', ()=>{
-  if(askCount === 5){
+  if(askCount === 7){
     ballAnswer.textContent = 'okay thats it!! you dont get to ask a question anymore..'
     askButton.style.visibility ='hidden';
 
-  }else if(askCount === 4){
+  } else if (askCount === 6){
     ballAnswer.textContent = 'okay you get one more chance'
     askCount ++;
-  }else if(askCount === 3){
+  } else if (askCount === 4){
     ballAnswer.textContent = 'for reals, come on you have to ask a question'
     askCount ++;
 
-  }else if(askCount === 2){
+  } else if (askCount === 2){
     ballAnswer.textContent = 'come on...really? ask me a question.'
     askCount ++;
   
-  }else if(!questionArea.value){
+  } else if (!questionArea.value){
     ballAnswer.textContent = 'please ask a question..'
     askCount ++;
 
-  }else{
+  } else {
     let randomIndex = randomNumber(answers.length);
     ballAnswer.textContent = answers[randomIndex].answer;
     questionArea.value = '';
